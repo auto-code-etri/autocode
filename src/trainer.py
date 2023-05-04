5469668d1bbf7edc141cb52fde99c1069a0feab3
DDIDUs <92288706+DDIDUs@users.noreply.github.com>
2023-05-04 14:35:41 +0900

    Loss mask update

--- src/trainer.py
+++ src/trainer.py
@@ -157,6 +157,50 @@ class Trainer:
         if self.main_process:
             self.summarywriter.close()
         return self.best_result if self.main_process else None
+    def sequence_mask(self, sequence_length, max_len=None):
+        if max_len is None:
+            max_len = sequence_length.data.max()
+        batch_size = sequence_length.size(0)
+        seq_range = torch.range(0, max_len - 1).long()
+        seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+        seq_range_expand = Variable(seq_range_expand)
+        if sequence_length.is_cuda:
+            seq_range_expand = seq_range_expand.cuda()
+        seq_length_expand = (sequence_length.unsqueeze(1)
+                             .expand_as(seq_range_expand))
+        return seq_range_expand < seq_length_expand
+
+
+    def compute_loss(self, logits, target, length):
+        """
+        Args:
+            logits: A Variable containing a FloatTensor of size
+                (batch, max_len, num_classes) which contains the
+                unnormalized probability for each class.
+            target: A Variable containing a LongTensor of size
+                (batch, max_len) which contains the index of the true
+                class for each corresponding step.
+            length: A Variable containing a LongTensor of size (batch,)
+                which contains the length of each data in a batch.
+        Returns:
+            loss: An average loss value masked by the length.
+        """
+
+        # logits_flat: (batch * max_len, num_classes)
+        logits_flat = logits.view(-1, logits.size(-1))
+        # log_probs_flat: (batch * max_len, num_classes)
+        log_probs_flat = functional.log_softmax(logits_flat)
+        # target_flat: (batch * max_len, 1)
+        target_flat = target.view(-1, 1)
+        # losses_flat: (batch * max_len, 1)
+        losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+        # losses: (batch, max_len)
+        losses = losses_flat.view(*target.size()).cuda()
+        # mask: (batch, max_len)
+        mask = self.sequence_mask(sequence_length=length, max_len=target.size(1))
+        losses = losses * mask.float().cuda()
+        loss = losses.sum() / length.float().sum()
+        return loss
 
     def _train_epoch(self, epoch: int) -> None:
         train_loss = AverageMeter()
@@ -184,18 +228,20 @@ class Trainer:
             if self.hparams.amp:
                 with torch.cuda.amp.autocast():
                     logit = self.model(src, tgt_input, src_mask, tgt_mask)
-                    loss = self.criterion(
-                        logit.contiguous().view(-1, logit.shape[-1]),
-                        tgt_label.contiguous().view(-1),
-                    )
-                    loss *= tgt_mask
+                    loss = self.compute_loss(logit, tgt_label, torch.LongTensor([len(i) for i in tgt_label]))
+                    #loss = self.criterion(
+                    #    logit.contiguous().view(-1, logit.shape[-1]),
+                    #    tgt_label.contiguous().view(-1),
+                    #)
+                    #loss *= tgt_mask
             else:
                 logit = self.model(src, tgt_input, src_mask, tgt_mask)
-                loss = self.criterion(
-                    logit.contiguous().view(-1, logit.shape[-1]),
-                    tgt_label.contiguous().view(-1),
-                )
-                loss *= tgt_mask
+                loss = self.compute_loss(logit, tgt_label, torch.LongTensor([len(i) for i in tgt_label]))
+                #loss = self.criterion(
+                #    logit.contiguous().view(-1, logit.shape[-1]),
+                #    tgt_label.contiguous().view(-1),
+                #)
+                #loss *= tgt_mask
             loss = loss.sum() / self.gradient_accumulation_step
 
             # update
